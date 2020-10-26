5644039f31ca2617147d7558c1429b16bd1175ae
takashi,hasegawa <1samuraiboy@gmail.com>
2020-10-26 16:46:03 +0900

    fix while->for

--- issue1.js
+++ issue1.js
@@ -1,15 +1,12 @@
-let num=1
-while (num < 100) {
+for (let num = 1; num < 100; num++){
   if (num % 3 == 0 && num % 5 == 0){
-      str="FizzBuzz"
-      console.log(str + " " + num)
+    str="FizzBuzz"
+    console.log(str + " " + num)
   } else if (num % 3 == 0) {
     str="Fizz"
     console.log(str + " " + num)
   } else if (num % 5 == 0) {
     str="Buzz"
     console.log(str + " " + num)
-  } else {
   }
-  num++
 }
