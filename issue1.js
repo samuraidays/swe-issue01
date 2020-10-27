1e7fbc4d92c1f9c1e8bd850dc27e40f725233b34
takashi,hasegawa <1samuraiboy@gmail.com>
2020-10-27 10:08:56 +0900

    add ;

--- issue1.js
+++ issue1.js
@@ -1,12 +1,12 @@
 for (let num = 1; num < 100; num++){
   if (num % 3 == 0 && num % 5 == 0){
-    str="FizzBuzz"
-    console.log(str + " " + num)
+    str="FizzBuzz";
+    console.log(str + " " + num);
   } else if (num % 3 == 0) {
-    str="Fizz"
-    console.log(str + " " + num)
+    str="Fizz";
+    console.log(str + " " + num);
   } else if (num % 5 == 0) {
-    str="Buzz"
-    console.log(str + " " + num)
+    str="Buzz";
+    console.log(str + " " + num);
   }
 }
