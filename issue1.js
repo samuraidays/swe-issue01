10b666786a5c29a01ad98a9728ed8470bd99a2f6
takashi.h <1samuraiboy@gmail.com>
2020-10-27 14:20:20 +0900

    fix == -> ===

--- issue1.js
+++ issue1.js
@@ -1,11 +1,11 @@
 for (let num = 1; num < 100; num++){
-  if (num % 3 == 0 && num % 5 == 0){
+  if (num % 3 === 0 && num % 5 === 0){
     str="FizzBuzz";
     console.log(str + " " + num);
-  } else if (num % 3 == 0) {
+  } else if (num % 3 === 0) {
     str="Fizz";
     console.log(str + " " + num);
-  } else if (num % 5 == 0) {
+  } else if (num % 5 === 0) {
     str="Buzz";
     console.log(str + " " + num);
   }
