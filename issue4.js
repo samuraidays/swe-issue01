2494f949ac422bcc2ad9a157dc0cb07bb18ac344
takashi,hasegawa <1samuraiboy@gmail.com>
2020-10-31 14:13:27 +0900

    to object

--- issue4.js
+++ issue4.js
@@ -1,24 +1,24 @@
-function showTask(tasks){
+function showTask(a){
     console.log("################");
     console.log("現在持っているタスク一覧");
     console.log("################");
 
-for (let i = 0; i < tasks.length; i++){
-    console.log(i+1 + ': ' + '[内容] ' + tasks[i][0] + ' [ジャンル] ' + tasks[i][1]);
+for (let i = 0; i < a.item.length; i++){
+    console.log(i+1 + ': ' + '[内容] ' + a.item[i] + ' [ジャンル] ' + a.genre[i]);
 }}
 
-var tasks = [
-    ["机を片付ける","掃除"],
-    ["牛乳を買う","買い物"],
-    ["散歩する","運動"],
-];
+let tasks = {
+    item: ["机を片付ける", "牛乳を買う", "散歩する"],
+    genre: ["掃除", "買い物", "運動"]
+}
+
 showTask(tasks);
 
 function addTask(tasks){
-    var task = [];
-    task[0] = prompt("タスクは？"),
-    task[1] = prompt("ジャンルは？")
-    tasks.push(task);
+    let item = prompt("タスクは？");
+    tasks.item.push(item);
+    let genre = prompt("ジャンルは？");
+    tasks.genre.push(genre);
 }
 
 while(true){    
